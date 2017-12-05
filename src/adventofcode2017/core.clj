5edca2b80b9a02709a2bd09c33655e70fcc93627
Weston Neal <weston@edatadivers.com>
2017-12-04 17:06:11 -0700

    Finished day 1 and started on day 2

--- src/adventofcode2017/core.clj
+++ src/adventofcode2017/core.clj
@@ -1,7 +1,7 @@
 (ns adventofcode2017.core
   (:gen-class))
 
-(defn day1
+(defn day1a
   "Solving a captcha"
   [input]
   ;; split the number to a string
@@ -16,3 +16,40 @@
                               in
                               ;; offset the list where the last item will wrap to the first
                               (conj (into [] (rest in)) (first in))))))))
+
+(defn day1b
+  "
+  Sum all the digits that meet the following condition: If the
+  current digit and the digit halfway around the circular list
+  is the same then 
+  "
+  [input]
+  (let [in (map #(Character/digit % 10) (str input))
+        offset (/ (count in) 2)]
+    ;; sum
+    (reduce +
+            ;; the first digit in each pair of adjacent digits
+            (map first
+                 ;; only if they are the same
+                 (filter #(apply = %)
+                         (map (fn [[idx value]]
+                                [value (nth in (mod (+ idx offset) (count in)))]) (map-indexed vector in)))))))
+
+(defn- process-line
+  [line]
+  (let [xs (map #(Integer/parseInt %)
+                (clojure.string/split line #" "))]
+    [(min xs) (max xs)]))
+
+
+(defn day2a
+  "
+  Calculate a checksum. The checksum is the sum of the
+  difference between the smallest and largest number of
+  each row
+  "
+  [input]
+  (let [min-maxes (map process-line (clojure.string/split-lines input))]
+    (reduce (fn [[min-val max-val] & args] (apply + (conj args (- max-val min-val))))
+            min-maxes)))
+            
--- test/adventofcode2017/core_test.clj
+++ test/adventofcode2017/core_test.clj
@@ -2,9 +2,22 @@
   (:require [clojure.test :refer :all]
             [adventofcode2017.core :refer :all]))
 
-(deftest day1-test
+(deftest day1a-test
   (testing "Testing Day 1"
-    (is (= (day1 1122) 3))
-    (is (= (day1 1111) 4))
-    (is (= (day1 1234) 0))
-    (is (= (day1 91212129) 9))))
+    (is (= (day1a 1122) 3))
+    (is (= (day1a 1111) 4))
+    (is (= (day1a 1234) 0))
+    (is (= (day1a 91212129) 9))))
+
+(deftest day1b-test
+  (testing "Testing Day 1"
+    (is (= (day1b 1212) 6))
+    (is (= (day1b 1221) 0))
+    (is (= (day1b 123425) 4))
+    (is (= (day1b 123123) 12))
+    (is (= (day1b 12131415) 4))))
+
+
+(deftest day2a-test
+  (testing "Testing Day 1"
+    (is (= (day2a "5 1 9 5\n7 5 3\n2 4 6 8") 18))))
